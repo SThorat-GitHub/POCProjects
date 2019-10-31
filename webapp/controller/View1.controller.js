18165c41634110c0867b72c6b3390a38ba11ff83
Smita Thorat <smita.thorat@invenio-solutions.com>
2019-10-31 08:43:57 +0000

    Smita First Commit
    Change-Id: I3b36d6a54b48d648adebc8ead375716b66b3b4ae

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sthorat.ST_Project1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sthorat/ST_Project1/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
